e7cc647858a85f996adb4ddbf329c28a419b7a13
RicardoArquimedes <richardcqt28@gmail.com>
2021-05-16 20:35:14 -0500

    adding style and display flex on header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,6 +31,7 @@ function Header() {
             <span>SERIES</span>
           </a>
         </NavMenu>
+        <UserImg src="https://lh3.googleusercontent.com/fife/ABSRlIp9GjxPZeaP9_2anHsvjkOMr87Q29uPaL839LoH3uB1Uh7Y2mmE6lM2p6cCVIcJNZaIgYa_fSpP853eDtx7CpEg5ynvAHQYz3GqbxNyr657KZR7Gjk1U4Lc69fkYKLdE8M3I4OUX-vZdKY8mYW2pht6afS5xm7tFnu1ZLJ0yFGNMyW9n4aA1-5gof68AC8weoZJ7KdY2HTcKTi0qInKUS4jUULC9m1BUNWEMpuMcTKhJ0WVDgL24P-nnoA_yZczl2gByuXbtH4bRMjwpCGcSxyZ5oc3uuVZtwdRBLoSZ0J8SqUX4flUAj3YwZzCHdfFxPVClV5bZF8duJiFiHNqbqF2vIKdikJrdwVli5v9rAVPaKGaegEM0VNpVSN_azmazGWTAMCW07x5FeIEUDNQBijRKHs4ZSh5oyt-rFJ9omxVNbOk32A__6yDukHO7wBTjUVHWg24OJ_nEx-fume18cX6aUI_t9irhWFSPpteJgeMUzWbDyUmvoJOenrU1gbx7Wn-JhIbhv9HQQ0Jrewj-hBGYEocIbjbzmX4rJY7IyF5KDufUQGeMSPgWSpOMar067TpGHjAgjGr0-0Jr0cHYDwn3_iv-nJxUJaTAYDDT3Quv7rbP88oN742-57D8IMhfsFSVdTDQ-_IheWvp8v2ctV0MGRJH25_z_zQembEa4CKz0KbRPPMddB6mQjE0a4yc620uJIJIvbGDRYnz6Nl5PDwB_BCPEbvz9UPWorfrcaMpQ=s64-c"/>
     </Nav>
   )
 }
@@ -50,11 +51,16 @@ const Logo = styled.img`
 `
 const NavMenu = styled.div`
     display: flex;
+    flex: 1;
+    margin-left: 25px;
+    align-items: center;
+    padding: 0 36px;
 
     a {
         display: flex;
         align-items: center;
         padding: 0 12px;
+        cursor: pointer;
 
         img{
             height: 20px;
@@ -63,6 +69,33 @@ const NavMenu = styled.div`
         span {
           font-size: 13px;
           letter-spacing: 1.42px;
+          position: relative;
+
+          &:after {
+            content: "";
+            height: 2px;
+            background: white;
+            position: absolute;
+            left: 0;
+            right:0;
+            bottom: -6px;
+            opacity: 0;
+            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45,0.94) 0s;
+            transform: scaleX(0);
+          }
+        }
+
+        &:hover{
+          span:after{
+            transform: scaleX(1);
+            opacity: 1;
+          }
+          
         }
     }
+`
+const UserImg = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
 `
