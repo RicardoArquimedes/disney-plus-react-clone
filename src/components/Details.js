aca4cff77cdae78f8075b35ac3cd49d850a8c898
RicardoArquimedes <richardcqt28@gmail.com>
2021-07-06 21:51:06 -0500

    updating movieData.json details database andMovies components

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,168 +1,221 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import { useParams } from 'react-router-dom'
-import db from '../firebase'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import db from "../firebase";
 
+const Detail = (props) => {
+  const { id } = useParams();
+  const [detailData, setDetailData] = useState({});
 
+  useEffect(() => {
+    db.collection("movies")
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setDetailData(doc.data());
+        } else {
+          console.log("no such document in firebase 🔥");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+  }, [id]);
 
+  return (
+    <Container>
+      <Background>
+        <img alt={detailData.title} src={detailData.backgroundImg} />
+      </Background>
 
-function Detail() {
-    const {id} = useParams();
-    const [ movie, setMovie ] = useState();
-   
-
-    useEffect(()=> {
-       //Grab the movie from DB
-       db.collection('movies')
-       .doc(id)
-       .get()
-       .then((doc)=> {
-         if (doc.exists) {
-             //save the movie data
-            setMovie(doc.data());
-
-         } else {
-           //redirects to homepage
-         }
-       })
-    }, [id])
-   
-    return (
-        <Container>
-            <Background>
-                 <img src={movie.backgroundImg} alt=""/> 
-            </Background>
-            <ImageTitle>
-                <img src={movie.titleImg} alt="" />
-            </ImageTitle>
-            <Controls>
-                <PlayButton>
-                    <img src="/images/play-icon-black.png" alt="" />
-                    <span>PLAY</span>
-                </PlayButton>
-                <TrailerButton>s
-                    <img src="/images/play-icon-white.png" alt=""/>
-                    <span>Trailer</span>
-                </TrailerButton>
-                <AddButton>
-                    <span>+</span>
-                </AddButton>
-                <GroupWatchButton>
-                    <img src="/images/group-icon.png" alt=""/>
-                </GroupWatchButton>
-            </Controls>
-            <SubTitle>
-               {movie.subTitle}
-            </SubTitle>
-            <Description>
-               {movie.description}
-            </Description>
-        </Container>
-    )
-}
-export default Detail
-
+      <ImageTitle>
+        <img alt={detailData.title} src={detailData.titleImg} />
+      </ImageTitle>
+      <ContentMeta>
+        <Controls>
+          <Player>
+            <img src="/images/play-icon-black.png" alt="" />
+            <span>Play</span>
+          </Player>
+          <Trailer>
+            <img src="/images/play-icon-white.png" alt="" />
+            <span>Trailer</span>
+          </Trailer>
+          <AddList>
+            <span />
+            <span />
+          </AddList>
+          <GroupWatch>
+            <div>
+              <img src="/images/group-icon.png" alt="" />
+            </div>
+          </GroupWatch>
+        </Controls>
+        <SubTitle>{detailData.subTitle}</SubTitle>
+        <Description>{detailData.description}</Description>
+      </ContentMeta>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-   min-height: calc(100vh - 70px);
-   padding: 0 calc(3.5vw + 5px);
-   position: relative;
-`
+  position: relative;
+  min-height: calc(100vh-250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+`;
 
 const Background = styled.div`
-   position: fixed;
-   top: 0;
-   left: 0;
-   bottom: 0;
-   right: 0;
-   z-index: -1;
-   opacity: 0.8;
-   img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-   }
-`
-
+  left: 0px;
+  opacity: 0.8;
+  position: fixed;
+  right: 0px;
+  top: 0px;
+  z-index: -1;
+  img {
+    width: 100vw;
+    height: 100vh;
+    @media (max-width: 768px) {
+      width: initial;
+    }
+  }
+`;
 
 const ImageTitle = styled.div`
-  height: 30vh;
-  width: 35vw;
+  align-items: flex-end;
+  display: flex;
+  -webkit-box-pack: start;
+  justify-content: flex-start;
+  margin: 0px auto;
+  height: 30vw;
   min-height: 170px;
-  min-weight: 200px; 
-  margin-top: 60px;     
- img {
-   width: 100%;
-   height: 100%;
-   object-fit: contain;
- }
-`
+  padding-bottom: 24px;
+  width: 100%;
+  img {
+    max-width: 600px;
+    min-width: 200px;
+    width: 35vw;
+  }
+`;
+
+const ContentMeta = styled.div`
+  max-width: 874px;
+`;
 
 const Controls = styled.div`
-   display: flex;
-   align-items: center;
-`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 24px 0px;
+  min-height: 56px;
+`;
 
-const PlayButton = styled.button`
-   border-radius: 4px;
-   font-size: 15px;
-   display: flex;
-   align-items: center;
-   height: 56px;
-   background: rgb(249, 249, 249);
-   border: none;
-   padding: 0px 24px;
-   margin-right: 22px;
-   letter-spacing: 1.8px;
-   cursor: pointer;
-   &:hover {
-     background: rgb(198, 198, 198);
-   }
-`
-
-const TrailerButton = styled(PlayButton)`
-  background: rgba(0, 0 , 0 ,0.3);
-  border: 1px solid rgb( 249, 249, 249);
-  color: rbg(249, 249, 249);
-  text-transform: uppercase;
-`
-
-
-
-
-const AddButton = styled.button`
-  width: 44px;
-  margin-right: 16px;
-  height: 44px;
+const Player = styled.button`
+  font-size: 15px;
+  margin: 0px 22px 0px 0px;
+  padding: 0px 24px;
+  height: 56px;
+  border-radius: 4px;
+  cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
+  letter-spacing: 1.8px;
+  text-align: center;
+  text-transform: uppercase;
+  background: rgb (249, 249, 249);
+  border: none;
+  color: rgb(0, 0, 0);
+  img {
+    width: 32px;
+  }
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+  @media (max-width: 768px) {
+    height: 45px;
+    padding: 0px 12px;
+    font-size: 12px;
+    margin: 0px 10px 0px 0px;
+    img {
+      width: 25px;
+    }
+  }
+`;
+
+const Trailer = styled(Player)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+`;
+
+const AddList = styled.div`
+  margin-right: 16px;
+  height: 44px;
+  width: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.6);
   border-radius: 50%;
   border: 2px solid white;
-  background-color: rgba(0, 0, 0, 0.6);
   cursor: pointer;
   span {
-    font-size: 30px;
-    color: white;
+    background-color: rgb(249, 249, 249);
+    display: inline-block;
+    &:first-child {
+      height: 2px;
+      transform: translate(1px, 0px) rotate(0deg);
+      width: 16px;
+    }
+    &:nth-child(2) {
+      height: 16px;
+      transform: translateX(-8px) rotate(0deg);
+      width: 2px;
+    }
   }
-`
-const GroupWatchButton = styled(AddButton)`
-   background: rgb(0, 0, 0);
-`
+`;
 
+const GroupWatch = styled.div`
+  height: 44px;
+  width: 44px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background: white;
+  div {
+    height: 40px;
+    width: 40px;
+    background: rgb(0, 0, 0);
+    border-radius: 50%;
+    img {
+      width: 100%;
+    }
+  }
+`;
 
 const SubTitle = styled.div`
   color: rgb(249, 249, 249);
   font-size: 15px;
   min-height: 20px;
-  margin-top: 26px;
-`
-
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
+`;
 
 const Description = styled.div`
   line-height: 1.4;
   font-size: 20px;
-  margin-top: 16px;
+  padding: 16px 0px;
   color: rgb(249, 249, 249);
-  max-width: 700px;
-`
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
+`;
+
+export default Detail;
